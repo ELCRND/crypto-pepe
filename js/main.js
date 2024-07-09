9e9045dfa3885f5b0409bcc0cefd9ec95c3b5a85
ELCRND <jwhh2a@gmail.com>
2024-07-09 16:52:51 +0300

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,5 @@
-// const menuBtn = document.querySelector(".header__menu-btn");
-// const menu = document.querySelector(".header__menu");
 const key = document.querySelector("#key");
-
 const tooltip = document.querySelector(".tooltip");
-
-// открыть/закрыть меню
-// menuBtn.addEventListener("click", () => {
-//   menu.classList.toggle("visible");
-//   menuBtn.classList.toggle("close");
-//   document.body.classList.toggle("scroll-off");
-// });
-
-// закрытие меню при нажатии на ссылку
-// document.querySelectorAll(".menu__link").forEach((link) =>
-//   link.addEventListener("click", () => {
-//     menu.classList.remove("visible");
-//     menuBtn.classList.remove("close");
-//     document.body.classList.remove("scroll-off");
-//   })
-// );
 
 // копирование кода в буфер обмена
 
